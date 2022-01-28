9c72ba458380d0af29966dda6d9a106cc94699f2
AlexandruTajamar21 <alexandru.preutesei@tajamar365.com>
2022-01-28 11:40:53 +0100

    Add project files.

---
+++ MvcCoreEmpleadosSession/Controllers/EmpleadosController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosSession.Extensions;
+using MvcCoreEmpleadosSession.Models;
+using MvcCoreEmpleadosSession.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEmpleadosSession.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaEmpleados()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+
+        public IActionResult SessionSalarios(int? salario)
+        {
+            if(salario != null)
+            {
+                int sumasalarial = 0;
+                if (HttpContext.Session.GetString("SUMASALARIAL")!= null)
+                {
+                    sumasalarial = int.Parse(HttpContext.Session.GetString("SUMASALARIAL"));
+                }
+                sumasalarial += salario.Value;
+                HttpContext.Session.SetString("SUMASALARIAL", sumasalarial.ToString());
+                ViewData["MENSAJE"] = "Salario Almacenado: " + salario.Value;
+            }
+            return View(this.repo.GetEmpleados());
+        }
+        public IActionResult SumarSalarios()
+        {
+            return View();
+        }
+
+        public IActionResult SessionEmpleados(int? idEmpleado)
+        {
+            if(idEmpleado != null)
+            {
+                Empleado emp = this.repo.FindEmpleado(idEmpleado.Value);
+                List<Empleado> empleadossession;
+                if(HttpContext.Session.GetObject<List<Empleado>>("EMPLEADOS") != null)
+                {
+                    empleadossession = HttpContext.Session.GetObject<List<Empleado>>("EMPLEADOS");
+                }
+                else
+                {
+                    empleadossession = new List<Empleado>();
+                }
+                empleadossession.Add(emp);
+                HttpContext.Session.SetObject("EMPLEADOS", empleadossession);
+                ViewData["MENSAJE"] = "Empleado " +
+                    emp.EmpNo + ", " + emp.Apellido + " almacenado en Session";
+            }
+            return View(this.repo.GetEmpleados());
+        }
+
+        public IActionResult EmpleadosAlmacenados()
+        {
+            return View();
+        }
+
+
+        public IActionResult SessionEmpleadosCorrecto(int? idEmpleado)
+        {
+            if (idEmpleado != null)
+            {
+                List<int> listaids;
+                if (HttpContext.Session.GetObject<List<int>>("IDSEMPLEADOS") == null)
+                {
+                    listaids = new List<int>();
+                }
+                else
+                {
+                    listaids = HttpContext.Session.GetObject<List<int>>("IDSEMPLEADOS");
+                }
+                listaids.Add(idEmpleado.Value);
+                HttpContext.Session.SetObject("IDSEMPLEADOS", listaids);
+                ViewData["MENSAJE"] = "Empleados Almacenados: " + listaids.Count();
+            }
+            return View(this.repo.GetEmpleados());
+        }
+
+        public IActionResult EmpleadosAlmacenadosCorrecto()
+        {
+            if (HttpContext.Session.GetString("IDSEMPLEADOS") == null)
+            {
+                ViewData["MENSAJE"] = "No Existen empleados almacenados";
+                return View();
+            }
+            else
+            {
+                List<int> listIdEmpleados = HttpContext.Session.GetObject<List<int>>("IDSEMPLEADOS");
+                List<Empleado> empleados = this.repo.GetEmpleadosSession(listIdEmpleados);
+                return View(empleados);
+            }
+        }
+    }
+}
---
+++ MvcCoreEmpleadosSession/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,37 @@
+﻿using MvcCoreEmpleadosSession.Data;
+using MvcCoreEmpleadosSession.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEmpleadosSession.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        EmpleadosContext context;
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            List<Empleado> empleados = this.context.Empleados.ToList();
+            return empleados;
+        }
+
+        public Empleado FindEmpleado(int idEmpleado)
+        {
+            return this.context.Empleados.SingleOrDefault(x => x.EmpNo == idEmpleado);
+        }
+
+        public List<Empleado> GetEmpleadosSession(List<int> idsEmpleados)
+        {
+            var consulta = from datos in this.context.Empleados
+                           where idsEmpleados.Contains(datos.EmpNo)
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
